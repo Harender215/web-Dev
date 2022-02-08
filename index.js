8e9536ea9992a01c062524161d2abe1a3078f5cd
harender-singh-chauhan <harendersingh7896@gmail.com>
2022-02-08 23:00:22 +0530

    js first line of codes

---
+++ index.js
@@ -0,0 +1,22 @@
+
+var a;
+
+ a = 10;
+ console.log(typeof a);
+ console.log(a);
+
+ a = "hello";
+ 'a'
+ console.log(a);
+
+
+ // type of operator-> it tells us the type of data stored in a variable
+ console.log(typeof a);
+
+ a = true;
+ console.log(a);
+ console.log(typeof a);
+
+ a = null;
+ console.log(a);
+ console.log(typeof a);
